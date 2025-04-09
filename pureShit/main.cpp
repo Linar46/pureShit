dd68f85afc540351cf5d2d8de66f43f8a36180d6
Linar46 <xioamialisher777@gmail.com>
2025-04-09 18:11:57 +0500

    working release

--- pureShit/main.cpp
+++ pureShit/main.cpp
@@ -35,16 +35,17 @@ public:
 
     void order(int& argc, char* argv[]) {
         std::cout << "BEGINING..." << std::endl;
-        if (argv[1] == "list") {
+        if (static_cast<std::string>(argv[1]) == "list") {
+            std::cout << "there" << std::endl;
             if (argc == 2) {
                 for (int i = 1; i <= lastID; i++) {
                     
-                    std::cout << i << " : " 
+                    std::cout << i << " : "
                         << data["tasks"][std::to_string(i)][0].get<std::string>()
-                        << " : " << data["tasks"][std::to_string(i)][1].get<std::string>();
+                        << " : " << data["tasks"][std::to_string(i)][1].get<std::string>() << std::endl;
                 }
             }
-            else if (argv[2] == "todo") {
+            else if (static_cast<std::string>(argv[2]) == "todo") {
                 for (int i = 1; i <= lastID; i++) {
                     if (data["tasks"][std::to_string(i)][1] == "todo") {
                         std::cout << "el: " << data["tasks"][std::to_string(i)][0].get<std::string>()
@@ -52,7 +53,7 @@ public:
                     }
                 }
             }
-            else if (argv[2] == "in-progress") {
+            else if (static_cast<std::string>(argv[2]) == "in-progress") {
                 for (int i = 1; i <= lastID; i++) {
                     if (data["tasks"][std::to_string(i)][1] == "in-progress") {
                         std::cout << "el: " << data["tasks"][std::to_string(i)][0].get<std::string>()
@@ -60,7 +61,7 @@ public:
                     }
                 }
             }
-            else if (argv[2] == "done") {
+            else if (static_cast<std::string>(argv[2]) == "done") {
                 for (int i = 1; i <= lastID; i++) {
                     if (data["tasks"][std::to_string(i)][1] == "done") {
                         std::cout << "el: " << data["tasks"][std::to_string(i)][0].get<std::string>()
@@ -68,39 +69,45 @@ public:
                     }
                 }
             }
-            
+
         }
-        else if (argv[1] == "add") {
-            add(argv[2]);
+        else if (static_cast<std::string>(argv[1]) == "add") {
+            add(static_cast<std::string>(argv[2]));
         }
-        else if (argv[1] == "delete") {
-            remove(argv[2]);
+        else if (static_cast<std::string>(argv[1]) == "delete") {
+            remove(static_cast<std::string>(argv[2]));
+
         }
-        else if (argv[1] == "update") {
+        else if (static_cast<std::string>(argv[1]) == "update") {
             std::string index(argv[2]);
             std::string content(argv[3]);
             data["tasks"][index][0] = content;
         }
-        else if (argv[1] == "mark-todo") {
+        else if (static_cast<std::string>(argv[1]) == "mark-todo") {
             std::string index(argv[2]);
             data["tasks"][index][1] = "todo";
         }
-        else if (argv[1] == "mark-in-progress") {
+        else if (static_cast<std::string>(argv[1]) == "mark-in-progress") {
             std::string index(argv[2]);
             data["tasks"][index][1] = "in-progress";
         }
-        else if (argv[1] == "mark-done") {
+        else if (static_cast<std::string>(argv[1]) == "mark-done") {
             std::string index(argv[2]);
             data["tasks"][index][1] = "done";
         }
+        std::cout << argv[1] << std::endl;
+    }
+    void test() {
+        //data["tasks"] = std::move(data["newtasks"]);
+        data.erase("newtasks");
     }
 };
 
 int main(int argc, char* argv[])
 {
     Tasks obj;
-    argv[1] = const_cast<char*>("add");
-    argv[2] = const_cast<char*>("groceries");
+    //argv[1] = const_cast<char*>("add");
+    //argv[2] = const_cast<char*>("groceries");
     
     //argv[1] = const_cast<char*>("list");
     //argc--;
@@ -109,6 +116,7 @@ int main(int argc, char* argv[])
     //argv[2] = const_cast<char*>("1");
     //std::cout << "what a fuck" << std::endl;
     obj.order(argc, argv);
-    
+    obj.test();
+
     return 0;
 }
